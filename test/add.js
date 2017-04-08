e6097e79c6392f804000d7ad620f1a5bd039ad23
Ryan Linton <ryan@infinite.red>
2017-04-07 20:55:16 -0700

    Add basic tests

--- test/add.js
+++ test/add.js
@@ -6,18 +6,18 @@ test('adds the proper npm module and component example', async t => {
   // spy on few things so we know they're called
   const addModule = sinon.spy()
   const addPluginComponentExample = sinon.spy()
-  
+  const exists = sinon.stub().returns(false)
+  const copy = sinon.spy()
+
   // mock a context
   const context = {
-    ignite: { addModule, addPluginComponentExample }
+    ignite: { addModule, addPluginComponentExample },
+    filesystem: { exists, copy }
   }
 
   await plugin.add(context)
-  
-  t.true(addModule.calledWith('react-native-MODULENAME', { link: true }))
-  t.true(
-    addPluginComponentExample.calledWith('PushNotificationExample.js', {
-      title: 'PushNotification Example'
-    })
-  )
+
+  t.true(addModule.calledWith('react-native-push-notification', { link: true }))
+  t.true(exists.called)
+  t.true(copy.called)
 })
--- test/remove.js
+++ test/remove.js
@@ -5,13 +5,18 @@ const plugin = require('../plugin')
 test('removes PushNotification', async t => {
   const removeModule = sinon.spy()
   const removePluginComponentExample = sinon.spy()
+  const confirm = sinon.stub().returns(true)
+  const remove = sinon.spy()
 
   const context = {
-    ignite: { removeModule, removePluginComponentExample }
+    ignite: { removeModule, removePluginComponentExample },
+    prompt: { confirm },
+    filesystem: { remove }
   }
 
   await plugin.remove(context)
 
-  t.true(removeModule.calledWith('react-native-MODULENAME', { unlink: true }))
-  t.true(removePluginComponentExample.calledWith('PushNotificationExample.js'))
+  t.true(removeModule.calledWith('react-native-push-notification', { unlink: true }))
+  t.true(confirm.called)
+  t.is(remove.args[0][0], `${process.cwd()}/App/Config/PushConfig.js`)
 })
